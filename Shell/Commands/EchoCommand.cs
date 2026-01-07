d4ec80b0f60911f3780b2930e7e1fb4903fcfa33
EBOju <ericbuschner+github@gmail.com>
2026-01-07 05:26:18 +0100

    Reverted changes to EchoCommand for testing

--- Shell/Commands/EchoCommand.cs
+++ Shell/Commands/EchoCommand.cs
@@ -6,7 +6,8 @@ public class EchoCommand : IBuiltInCommand
 
     public void Execute(List<string> args)
     {
-        string message = string.Join(' ', args).Replace("'", string.Empty);
+        string message = string.Join(' ', args);
+        //string message = string.Join(' ', args).Replace("'", string.Empty);
         Console.WriteLine(message);
     }
 }
--- src/main.cs
+++ src/main.cs
@@ -72,9 +72,13 @@ class Program
                 // toggle inSingleQuote flag
                 inSingleQuote = !inSingleQuote;
             }
+            else
+            {
+                // add character to current argument
+                currentArg += commandString[i];
+            }
 
-            // add character to current argument
-            currentArg += commandString[i];
+            //currentArg += commandString[i];
 
             // if we hit a space and we're not in single quotes, or we're at the end of the string, finalize the current argument
             if (i == commandString.Length - 1 || (commandString[i] == ' ' && !inSingleQuote))
